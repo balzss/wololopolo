91e6e96ac91b58f16a4eac85e9b8195e37150925
balazs.saros <balazs.saros@gmail.com>
2018-06-19 12:41:13 +0200

    fixed rendering issue

--- client-side/main.js
+++ client-side/main.js
@@ -1,51 +1,54 @@
-let canvas = document.querySelector('canvas');
-let ctx = canvas.getContext('2d');
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
 
-ctx.globalCompositeOperation = 'multiply';
+drawPolo(canvas.width, context);
 
-let img = document.querySelector('img');
-ctx.drawImage(img, 0, 0, 600, 600);
+function drawPolo(canvasWidth, ctx) {
+    context.globalCompositeOperation = 'multiply';
+    context.drawImage(document.querySelector('img'), 0, 0, 600, 600);
 
-const longText = 'ez egy hosszu szoveg, amit ki kellene irni hogy tudja tesztelni ezt a szarrt'.split(' ');
-const targetWidth = 160;
-const linePadding = 4;
-const targetChars = 18;
+    const longText = 'ugytunik hogy csak a kirajzolas koordinataival volt valami baj'.split(' ');
+    const targetWidth = 160;
+    const linePadding = 4;
+    const targetChars = 12;
+    const lineHeight = 1.2;
 
-let yOffset = 160;
-let cursor = 0;
-let wordCount = 1;
-let fontSize = 60;
-let textBuffer = '';
-
-while (true) {
-    wordCount = 1;
-    fontSize = 60;
-
-    if (cursor >= longText.length) break;
+    let yOffset = 160;
+    let cursor = 0;
+    let wordCount = 1;
+    let fontSize = 60;
+    let textBuffer = '';
 
     while (true) {
-        textBuffer = longText.slice(cursor, wordCount).join(' ');
-        console.log(longText.slice(cursor, wordCount));
-        if (textBuffer.length >= targetChars || (cursor + wordCount) > longText.length) {
-            cursor += wordCount;
-            break;
-        } else {
-            wordCount++;
+        wordCount = 1;
+        fontSize = 40;
+
+        if (cursor >= longText.length) break;
+
+        while (true) {
+            textBuffer = longText.slice(cursor, cursor + wordCount).join(' ');
+            console.log(longText.slice(cursor, cursor + wordCount));
+            if (textBuffer.length >= targetChars || (cursor + wordCount) > longText.length) {
+                cursor += wordCount;
+                break;
+            } else {
+                wordCount++;
+            }
         }
-    }
 
-    while (true) {
-        ctx.font = fontSize + 'px serif';
-        ctx.fillStyle = 'rgb(255,0,255)';
-        const textSize = ctx.measureText(textBuffer);
+        while (true) {
+            ctx.font = fontSize + 'px/' + lineHeight + 'em sans-serif';
+            ctx.fillStyle = 'rgb(255,0,255)';
+            const textWidth = ctx.measureText(textBuffer).width;
 
-        console.log(textBuffer);
-        if (textSize.width <= targetWidth) {
-            ctx.fillText(textBuffer, canvas.width / 2 - textSize.width / 2, yOffset);
-            yOffset += fontSize + linePadding;
-            break;
-        } else {
-            fontSize--;
+            console.log(textBuffer);
+            if (textWidth <= targetWidth) {
+                ctx.fillText(textBuffer, canvasWidth / 2 - textWidth / 2, yOffset);
+                yOffset += fontSize * lineHeight;
+                break;
+            } else {
+                fontSize--;
+            }
         }
     }
 }
